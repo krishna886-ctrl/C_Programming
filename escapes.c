eac6b685a1579c7b9a8fd72c0a9d71ddb45326b0
krishna886-ctrl <120310214+krishna886-ctrl@users.noreply.github.com>
2023-02-18 22:28:50 +0530

    Add files via upload

---
+++ escapes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(){
+	/* \n */
+	/*printf("Hello\nC Programming");*/
+	/*Output: Hello
+              C Programming
+              
+	/* \t */
+    /* printf("Hare\tKrishna!");*/
+    /* Output: Hare    Krishna!*/
+    
+    /* \r:removes pre words */
+    /* printf("Hello\rWorld");
+    /* Output: World*/
+    
+    /* \b:remove one last word only */
+    /* printf("Bhakti\bMarga!");*/
+    /* Output: BhaktMarga!*/
+    
+    /* \a:alert create such sound in for 1sec. */
+    /* printf("Hello\aPython"); */
+    /* Output: HelloPython*/
+    
+    /* // */
+    /* printf("\\n"); */
+    /* Output: \n */
+    
+    /*Question*/
+    /* printf("Jai GLA\n Hello GLA\bHello\rworld");*/
+    /* Output:Jai GLA
+              worldo GLHello*/
+    
+    }
+
+
+
+
